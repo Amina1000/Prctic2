2a2157f77f9fe4419ca5a56a74f480cc191914b6
amina1000 <amina1000@yandex.ru>
2021-02-22 16:01:48 +0300

    Generic/Параметризация (дополнительный курс. урок 28)

---
+++ src/lesson2/Main.java
@@ -0,0 +1,13 @@
+package lesson2;
+
+public class Main {
+    /**
+     * homework lesson 2
+     *
+     * @author Isaeva Amina
+     * date 22.02.2021
+     */
+    public static void main(String[] args) {
+
+    }
+}
