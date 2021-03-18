1413da705ec696810d60d9adf6c61261345ccc59
amina1000 <amina1000@yandex.ru>
2021-03-18 23:11:04 +0300

    Урок 4. Задача 1

---
+++ src/lesson4/Main.java
@@ -0,0 +1,13 @@
+package lesson4;
+
+/**
+ * homework lesson4
+ *
+ * @author Admin
+ * 17.03.2021
+ */
+public class Main {
+    public static void main(String[] args) {
+        new  MainWindow();
+    }
+}
